fa1463f7769c5669270e0bb7dbbf1a41b36b4b2b
Sturm,M.J. (Martijn) <m.j.sturm@students.uu.nl>
2019-09-23 13:40:23 +0200

    Update MAGGIC.py
    Added regex patterns for numeric MAGGIC variables

--- negation/MAGGIC.py
+++ negation/MAGGIC.py
@@ -1,7 +1,8 @@
 # %%
 import negation.preprocess_neg
 import negation.lexicon
-
+# import re
+import pandas as pd
 
 # %% Load lexicon from Excel
 maggic1 = negation.preprocess_neg.import_excel("MAGGIC targets.xlsx")
@@ -9,31 +10,76 @@ print(maggic1)
 
 # %% Generate regex pattern for synonyms:
 maggic2 = negation.lexicon.gen_regex(maggic1)
-print(maggic2)
+# print(maggic2)
+# print(maggic2.columns)
+
+# %% SBP:
+# Captures a 2 or 3 digit number
+
+# Synonym preceding sbp:
+sbp_regex1 = \
+    r"([^(a-z)]rr[^(a-z)]|mmhg|bloeddruk|tensie|" \
+    r"riva-rocci|mm\shg|systole|systolisch)" \
+    r"(.{1,5})(\d{2,3})(/(\d{2})?)"
+
+# sbp preceding synonym:
+sbp_regex2 = \
+    r"(\d{2,3})(/(\d{2})?)(.{1,5})" \
+    r"([^(a-z)]rr[^(a-z)]|mmhg|bloeddruk|tensie|" \
+    r"riva-rocci|mm\shg|systole|systolisch)"
+
+# Add SBP row to MAGGIC dataframe:
+maggic2.loc[len(maggic2.index)] = \
+    pd.Series({"Type": "sbp1", "Lex": "sbp", "Regex": sbp_regex1})
+maggic2.loc[len(maggic2.index)] = \
+    pd.Series({"Type": "sbp2", "Lex": "sbp", "Regex": sbp_regex2})
+
+# %% Variable Ejection Fraction
+
+# Synonym preceding percentage
+# With a margin of 10 characters between synonym and value
+vef_regex1 = \
+    r"(LVEF|EF|ejectiefractie|linkerventrikel\sejectiefractie|" \
+    r"linkerventrikelejectiefractie|lv\sejectiefractie|" \
+    r"linker\sventrikel\sejectie\sfractie|kamerfunctie)" \
+    r"(.{1,10})\d{2}"
+
+# Synonym folowing percentage
+# With margin of 10 characters after percentage
+vef_regex2 = \
+    r"\d{2}%?.{1,10}" \
+    r"(LVEF|EF|ejectiefractie|linkerventrikel\sejectiefractie|" \
+    r"linkerventrikelejectiefractie|lv\sejectiefractie|" \
+    r"linker\sventrikel\sejectie\sfractie|kamerfunctie)"
+
+# Testing:
+# # first synonym:
+# first_syn_string = "Blabla blabla. Bla bla EF is 45 % blabla. blabla"
+# print(first_syn_string)
+# print(vef_regex1)
+# print(re.search(pattern=vef_regex1, string=first_syn_string))
+
+# # First value:
+# first_val_string = "test test. 23% vind niet linkerventrikel ejectiefractie"
+# print(first_val_string)
+# print(vef_regex2)
+# print(re.search(pattern=vef_regex2, string=first_val_string))
+
+# Add VEF row to MAGGIC dataframe
+maggic2.loc[len(maggic2.index)] = \
+    pd.Series({"Type": "vef1", "Lex": "vef", "Regex": vef_regex1})
+maggic2.loc[len(maggic2.index)] = \
+    pd.Series({"Type": "vef2", "Lex": "vef", "Regex": vef_regex2})
 
 # %% Add column with calc = "MAGGIC"
-maggic2["calc"] = "MAGGIC"
+# maggic2["calc"] = "MAGGIC"
+maggic2["Direction"] = "''"
+maggic2["Comments"] = "''"
+
 # %% Convert pandas to yaml format
 negation.lexicon.panda_to_yaml("MAGGIC.yml", maggic2)
 
-# %% Manual numeric variables:
-# Systolic blood pressure:
-
-# Capture two numbers separated by
-# a forward slash with 1 or 0 whitespace flanking the sbp:
-sys = r"(?<=\s)\d{2,3}(?=/\d{2,3}\s?(aap))"
-dias = r"/\d{2,3}"
-prec = r"\s"
-fol = r""
-sbp_syns = r""
-sbp_tot = r"(?<=)"
-
-# capture the systolic blood pressure when noted in 120/80 format
-# With possible whitespaces surounding the forward slash
-sbp2 = r"(\d{2,3})(?=/(\d{2})"
-
-# Variable Ejection Fraction
-# Percentage:
-perc = r"\s\d{2}%?\s"
+# # %%
+# print(re.search(pattern="a.{1,3}bc", string="ddappbcddef"))
 
 # %%
