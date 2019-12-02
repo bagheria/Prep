a904340203362b89029c7b880b85ccf54234f4ef
Sturm,M.J. (Martijn) <m.j.sturm@students.uu.nl>
2019-12-02 17:49:07 +0100

    Update risk_vars.py
    add method to set self.subClass
    add method to:
    - generate self._modInfo dataframe
    - set self.mod_conflict
    add method getDataframe

--- negation/risk_vars.py
+++ negation/risk_vars.py
@@ -1,14 +1,17 @@
 import negation.modifiers as modifiers
 from abc import ABC, abstractmethod
 import re
+import pandas as pd
 
 class RiskVar(ABC):
-    
+    _df_atr_exclude = ["object", "mod"]
+
     def __init__(self, object):
         self.object = object
         self.literal = object.getLiteral()
         self.cat = object.categoryString()
         self.phrase = object.getPhrase()
+        self._setSubClass()
 
         # Modification initialization
         self.mod = []
@@ -27,6 +30,9 @@ class RiskVar(ABC):
     def __eq__(self, other):
         pass
 
+    @abstractmethod
+    def _setSubClass(self):
+        pass
 
     # @abstractmethod
     # def getOverview(self):
@@ -38,13 +44,13 @@ class RiskVar(ABC):
 
     # All methods that must be called after object construction
     # and modifier addition
-    # def processInfo(self):
-    #     """processes object and mod information after complete construction
-    #     of object"""
-    #     # If self.mod is not empty:
-    #     if self.mod:
-    #         self._processMod()
-    #     self._analyseNeg()
+    def processInfo(self):
+        """processes object and mod information after complete construction
+        of object"""
+        # If self.mod is not empty:
+        if self.mod:
+            self._processMod()
+        # self._analyseNeg()
 
     def getModInfo(self):
         dict = {}
@@ -60,6 +66,34 @@ class RiskVar(ABC):
             "mods" : self.getModInfo()}
         return(dict)
 
+    
+    def getDataframe(self):
+        # Add atributes that are not in exclude list to dataframe
+        incl = list(set(vars(self)) - set(self._df_atr_exclude))
+        atr_df = pd.DataFrame()
+        for atr in incl:
+            if atr[0] != "_":
+                # print(atr, getattr(self, atr))
+                atr_series = pd.Series(data=getattr(self, atr), name=atr)
+                atr_df.insert(0, atr_series.name, atr_series, True)
+        atr_df = atr_df.add_prefix("var_")
+
+        # Gather mod info and put in dataframe
+        mod_df = pd.DataFrame()
+        for mod in self.mod:
+            mod_df = mod_df.append(mod.getDataframe(), ignore_index=True, sort=False)
+        # mod_df = mod_df.add_prefix("mod_")
+
+        # combine both in dataframe:
+        
+        if len(mod_df.index > 0):
+            df = pd.DataFrame()
+            # Replicate atr_df rows to match number of mods
+            atr_df = pd.concat([atr_df]*len(mod_df.index), ignore_index=True)
+            # Join both dataframes:
+            df = atr_df.join(mod_df, on=None, how='outer', sort=False)
+            return(df)
+        return(atr_df)
     # # Check for modification
     # def _processMod(self):
     #     """Processes modifier information:
@@ -77,6 +111,54 @@ class RiskVar(ABC):
     #             raise Exception(
     #                 "Mod not recognized as one of valid options",
     #                 mod, mod.phrase, mod.type, mod.literal)
+
+    _modSubClasses = ["negation", "date", "temporality", "examination"]
+    def _processMod(self):
+        info = {}
+
+        # Function to produce column for self._modInfo df
+        def base(start):
+            base = {}
+            for subclass in self._modSubClasses:
+                base.update({subclass : start})
+            return(base)
+        
+        # Frequency
+        freq = base(int())
+        for mod in self.mod:
+            freq[mod.subClass] = freq[mod.subClass] + 1
+        
+        # Conflict
+        conf = base(bool())
+        # compare every mod 
+        length = len(self.mod)
+        for i in range(length):
+            for j in range(i+1, length):
+                # First check if same subclass
+                if self.mod[i].subClass == self.mod[j].subClass:
+                    # If same subclass, but different mod value
+                    match = (self.mod[i] == self.mod[j])
+                    # Set true if conflict (match = False)
+                    if not match:
+                        conf[self.mod[i].subClass] = True
+
+        # Puts key in list if value is True
+        conf_list = [key for key, value in conf.items() if value]   
+        if conf_list:
+            self.mod_conflict = conf_list
+        else:
+            self.mod_conflict = False
+                        
+        # Add all to info dictionary
+        info.update({
+            "frequency" : freq,
+            "conflict" : conf})
+
+        self._modInfo = pd.DataFrame(info)
+
+        # df = pd.DataFrame()
+        # for subclass in self._modSubClasses:
+        #     row = pd.Series(name = "Subclass", data = subclass)
 
     # def _analyseNeg(self):
     #     """Checks negation scores of finding.
@@ -122,12 +204,15 @@ class NumVar(RiskVar):
     def __eq__(self, other):
         return(self.value == other.value)
 
+    def _setSubClass(self):
+        self.subClass = "numeric"
+
     def processInfo(self):
         super().processInfo()
-        self._getValue()
-        self._conflictValue()
-        self._processValue()
-        self._setResult()
+        # self._getValue()
+        # self._conflictValue()
+        # self._processValue()
+        # self._setResult()
 
     def _getValue(self):
         if self.cat == "vef":
@@ -217,9 +302,12 @@ class BinVar(RiskVar):
             raise Exception("__eq__ evaluation unseen outcome of:\n",
                 self.negation,"\n", other.negation)
 
+    def _setSubClass(self):
+        self.subClass = "binary"
+
     def processInfo(self):
         super().processInfo()
-        self._setResult()
+        # self._setResult()
     
     def _setResult(self):
         self.result = {"negation" : self.negation["polarity"]}
@@ -248,11 +336,14 @@ class FactVar(RiskVar):
             return True 
         else:
             return False
-    
+
+    def _setSubClass(self):
+        self.subClass = "factorial"
+
     def processInfo(self):
         super().processInfo()
-        self._processFactor()
-        self._setResult()
+        # self._processFactor()
+        # self._setResult()
 
     def _processFactor(self):
         self.factor = self.literal    
