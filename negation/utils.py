169eb61c2396fe0288e48e74f972c7d6a530a2b8
Sturm,M.J. (Martijn) <m.j.sturm@students.uu.nl>
2020-01-20 11:00:21 +0100

    Update utils.py
    add reorder columns method
    add dict hack to dataframe function

--- negation/utils.py
+++ negation/utils.py
@@ -54,3 +54,43 @@ def export_excel(df, filename):
     filepath = Path.cwd() / "negation" / "output" / filename
     df.to_excel(filepath, index=False)
     print(f"Excel file '{filename}' was created in {filepath}")
+
+
+def df_dict_hack(master_dict):
+    # Write function that appends every item to a dataframe
+    # and returns the dataframe eventually but
+    # if a dictionary is among the items, this should be included 
+    # in the dataframe, and the dataframe should be expanded accordingly
+    
+    # For every subdict in a dict, make a row per subdict, and copy dict values
+    # to every row
+    subdicts = []
+    for value in master_dict.value():
+        if isinstance(value, dict()):
+            subdicts.append(value)
+    
+    df = pd.DataFrame()
+
+    for j in range(10):
+        df = df.append({'key_%s'%random.choice('abcde'): j}, ignore_index=True)
+    
+    # for key, value in dictionary.items():
+    #     insertion = pd.Series(data=value, name=key)
+    #     df.insert(0, insertion.name, insertion, True)
+
+    return(df)
+
+def df_reorder_cols(df, contains_str, pos):
+    if pos != "first" or "last":
+        raise Exception("'pos' argument should be 'first' or 'last'") 
+   
+    # identify column types:
+    all_cols = df.columns.values
+    spec_cols = [col for col in all_cols if contains_str in col]
+    other_cols = [col for col in all_cols if contains_str not in col]
+
+    # change column order:
+    if pos == "last":
+        return(df.reindex(columns=other_cols + spec_cols))
+    else:
+        return(df.reindex(columns=spec_cols + other_cols))
